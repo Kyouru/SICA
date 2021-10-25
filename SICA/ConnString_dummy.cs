23a0bf8efbee5f6a2dd9c2ff8f581e36039a7431
Kyouru <ren.kyouru@gmail.com>
2021-10-25 18:47:04 -0500

    Remover ficheros innecesarios

---
+++ SICA/ConnString_dummy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SICA
+{
+    class ConnString
+    {
+        public string sqlserversica = "SQLSERVER CONNECTION STRING";
+        public string GetString(string _name)
+        {
+            try
+            {
+                return (string)typeof(ConnString).GetField(_name).GetValue(this);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+    }
+}
